7529f4446857bc1c637fd7a6f1fe4e0c087b5781
Ilija Radosavovic <ilija.radosavovic@gmail.com>
2022-10-08 13:36:42 -0700

    Import isaacgym before torch (if present)

--- mvp/__init__.py
+++ mvp/__init__.py
@@ -1,1 +1,6 @@
+# Ensure that IsaacGym is imported before PyTorch (if present)
+try:
+    from isaacgym import gymapi
+except ModuleNotFoundError:
+    pass
 from mvp.backbones.model_zoo import available_models, load
